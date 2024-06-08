155edd5e19fc49280dbc5943617ec83c1b799d8c
AntToeeKnee <124924118+AntToeeKnee@users.noreply.github.com>
2024-06-08 00:02:13 -0700

    Added dynamically changing display and values of numbers chosen/operator selected are now stored

--- js-file.js
+++ js-file.js
@@ -1,3 +1,7 @@
+let wholeString = "";
+let valueOne;
+let valueTwo;
+let valueOperator;
 
 function add(x, y) {
     return x + y;
@@ -17,8 +21,27 @@ function divide(x, y) {
 
 
 function appendSym(x) {
+    if(valueOperator === "+" || valueOperator === "*" || valueOperator === "/" || valueOperator === "-") {
+        clearScreen();
+        valueOperator = "";
+    }
     let oldText = document.getElementById("number-display").textContent;
     let newText = oldText + x;
+    wholeString = wholeString + x;
     document.getElementById("number-display").textContent = newText;
+    console.log(wholeString);    
     
+}
+
+function appendOpp(y) {
+    clearScreen();
+    wholeString = wholeString + y;
+    document.getElementById("number-display").textContent = y;
+    valueOperator = document.getElementById("number-display").textContent;
+    console.log(wholeString);    
+
+}
+
+function clearScreen() {
+    document.getElementById("number-display").textContent = "";
 }
